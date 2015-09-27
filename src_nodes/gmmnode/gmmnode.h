bc33ccf3af20985e01f6bbb483cf8d0277ec9c35
Dinesh Vaithyalingam Gangatharan <dineshvg1023@gmail.com>
2015-09-27 22:19:20 +0200

    Add GMM logic to train data from table

--- src_nodes/gmmnode/gmmnode.h
+++ src_nodes/gmmnode/gmmnode.h
@@ -10,6 +10,7 @@
 #include "tabledata/tabledata.h"
 #include <QObject>
 #include <QString>
+#define MAX_SEGMENTS        40
 
 
 class CGmmNode: public CNode
@@ -23,7 +24,36 @@ class CGmmNode: public CNode
     QSharedPointer<CRulesetData> m_ruleset;
     QSharedPointer<const CTableData> gmm_train_table;
     QSharedPointer<const CTableData> gmm_test_table;
-    //QList<QSharedPointer<QVariant>> trainingData;
+    /**
+    * Declaration for data training
+    */
+     QList<float> data_points;
+     qint32 segmentNum = 3;
+     qint32 numOfData;
+     float *orig_vector, *in_vector,*labs;//original_vector, input_vector, labels
+     float datamin  = 1.0E30, datamax  = -1.0E30, ival, pmax, mlikedenom, p;
+     double loglik_old, loglik_new,dsegment_number,conv_crit, segment_probs_total,
+            mu_numer, mu_denom, sd_numer, sd_denom, loglik_sum;
+     double mu[MAX_SEGMENTS],sd[MAX_SEGMENTS], segment_probs[MAX_SEGMENTS], denom[256];
+     double delta_lookup[256][MAX_SEGMENTS], dnorm_lookup[256][MAX_SEGMENTS];
+     int max_iterations, segment_number =6, empty_levels, current_breakpoint,
+         initialization_error,current_total, done, initialization_multiplier,
+         iter_counter;
+     int sd_counter[MAX_SEGMENTS], mu_counter[MAX_SEGMENTS],  greyscale_histogram[256],
+         initial_breakpoints[(MAX_SEGMENTS+1)], cardinality[MAX_SEGMENTS];
+
+    /**
+    * Declaration for data testing
+    */
+    QList<float> test_data_points;
+    qint32 numOfTestData;
+    float *test_vector, *in_test_vector, *test_labs;
+    /**
+    * Flags for training and testing data
+    */
+    bool test = false;
+    bool train = false;
+    bool testInFile = false;
 
   public:
     // Constructor
@@ -36,13 +66,15 @@ class CGmmNode: public CNode
     virtual bool start();
     // Receive data sent by other nodes connected to this node.
     virtual bool data(QString gate_name, const CConstDataPointer &data);
+    //Converts the table data into a list
+    //QList<QString> extractListFromTable(const QSharedPointer<const CTableData> &table);
     // Trains the data
     void trainData(const QSharedPointer<const CTableData> &table);
     // Test the data
     void testData(const QSharedPointer<const CTableData> &table);
     //Gaussian Mixture model to train the data
     void gmmTrain(const QList<qint32> &training_data);
-   // virtual float *vector(int n0,int n);
+    float* vector(int n0,int n);
     float Normi(int x, float mu, float sd);
     float Normr(float x, float mu, float sd);
     inline qint32 rnd() const;
@@ -51,10 +83,10 @@ class CGmmNode: public CNode
      //              const QString &filename);
 };
 
-qint32 CGmmNode::rnd() const
-{
+//qint32 CGmmNode::rnd() const
+//{
     // 30-31 bit random number
-    return (qrand() + (qrand() << 15)) & 0x7fffffff;
-}
+  //  return (qrand() + (qrand() << 15)) & 0x7fffffff;
+//}
 
 #endif // GMMNODE_H
